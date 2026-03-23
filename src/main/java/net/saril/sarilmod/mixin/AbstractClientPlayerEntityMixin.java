dfd7c81e49283ce3eee6aecc6370022e897dca0e
MonkeyBoi <monkeylaguri308@gmail.com>
2026-03-22 21:01:02 -0400

    Bow Fov Fix

---
+++ src/main/java/net/saril/sarilmod/mixin/AbstractClientPlayerEntityMixin.java
@@ -0,0 +1,18 @@
+package net.saril.sarilmod.mixin;
+
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import net.minecraft.client.network.AbstractClientPlayerEntity;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.saril.sarilmod.item.ModItems;
+import org.spongepowered.asm.mixin.Mixin;// Created by 7410
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(AbstractClientPlayerEntity.class)
+public abstract class AbstractClientPlayerEntityMixin {
+    @WrapOperation(method = "getFovMultiplier", at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemStack;isOf(Lnet/minecraft/item/Item;)Z"))
+    private boolean changeFovForCustomBowToo(ItemStack instance, Item item, Operation<Boolean> original) {
+        return original.call(instance, item) || original.call(instance, ModItems.SOLAR_MATTER_BOW);
+    }
+}
